bba9ab30b9179ed9b25822f507b92d99f9871bdf
azetner <adrian.zetner@canada.ca>
2023-09-14 13:27:08 -0500

    worknotes initial from backup

--- scripts/update-weekly.sh
+++ scripts/update-weekly.sh
@@ -1,4 +1,5 @@
 #! usr/bin/bash
 
+# This script updates the quick link to the previous weekly note on creation of a new weekly note
 latest=$(ls -1 journal/`date +%Y`/ | tail -n 1 | sed 's/\.md//g')
 sed -i "s/\[\[.*Weekly Note\]\]/\[\[$latest\|Weekly Note\]\]/g" _/Quick\ Links.md
